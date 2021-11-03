7ffdf8068e571fab1bb49f11edffda4979f92e40
HominSu <1774069959@qq.com>
2021-11-03 17:37:46 +0800

    👷 finish main.cc

--- main.cc
+++ main.cc
@@ -6,6 +6,8 @@
 #include <list>
 #include <filesystem>
 
+#include "task/file_crypt.h"
+
 int main(int _argc, char *_argv[]) {
   if (_argc != 5) {
     std::cerr << "para error!" << std::endl;
@@ -37,12 +39,23 @@ int main(int _argc, char *_argv[]) {
     std::filesystem::create_directories(dst_dir);
   }
 
+  // 线程安全的内存资源
+  auto memory_resource = std::make_shared<std::pmr::synchronized_pool_resource>();
+
   // 文件列表
-  auto files_entry = std::list<std::filesystem::directory_entry>();
+  auto file_crypt_list = std::list<std::shared_ptr<FileCrypt>>();
 
   // 如果是文件就直接 append 到文件列表
   if (std::filesystem::is_regular_file(src_dir)) {
-    files_entry.emplace_back(src_dir);
+    std::filesystem::directory_entry it(src_dir);
+
+    auto file_crypt = std::make_shared<FileCrypt>();
+
+    file_crypt->Start(it.path().string(),
+                      dst_dir + "/" + it.path().filename().string(),
+                      password,
+                      is_encrypt,
+                      memory_resource);
   } else if (std::filesystem::is_directory(src_dir)) {
     // 遍历目录中的文件
     for (auto &iter: std::filesystem::directory_iterator(src_dir)) {
@@ -50,12 +63,23 @@ int main(int _argc, char *_argv[]) {
       if (!iter.is_regular_file()) {
         continue;
       }
-      files_entry.push_back(iter);
+
+      auto file_crypt = std::make_shared<FileCrypt>();
+
+      file_crypt->Start(iter.path().string(),
+                        dst_dir + "/" + iter.path().filename().string(),
+                        password,
+                        is_encrypt,
+                        memory_resource);
     }
   }
 
-  for (auto &file_it: files_entry) {
-    std::cout << file_it.path().string() << std::endl;
+  // 等待任务执行完成
+  size_t task_num = 0;
+  for (auto &file_crypt: file_crypt_list) {
+    file_crypt->Wait();
+    std::cout << ++task_num << ": in: [" << file_crypt->in_file_ << "], out: [" << file_crypt->out_file_ << "]"
+              << std::endl;
   }
 
   return 0;
